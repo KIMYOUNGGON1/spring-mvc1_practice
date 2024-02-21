e471f0ef22681ce7bc2c319b6a95d130074c19c8
KIMYOUNGGON1 <dudrhs950@naver.com>
2024-02-22 08:35:46 +0900

    20240222 수업내용 - HTTP 요청 파라미터 - @ModelAttribute

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,9 +1,11 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,10 +28,10 @@ public class RequestParamController {
     }
 
     @ResponseBody
-    @RequestMapping("/request-param-v2")
+    @RequestMapping ( "/request-param-v2" )
     public String requestParamV2(
-            @RequestParam("username") String memberName,
-            @RequestParam("age") int memberAge
+            @RequestParam ( "username" ) String memberName,
+            @RequestParam ( "age" ) int memberAge
     ) {
         log.info("username={}, age={}", memberName, memberAge);
         return "ok";
@@ -42,10 +44,10 @@ public class RequestParamController {
     //    애노테이션에 이름을 생략하지 않고 다음과 같이 이름을 항상 적어준다. **이 방법을 권장한다.**
     //            `@RequestParam("username") String username` `@PathVariable("userId") String userId
     @ResponseBody
-    @RequestMapping("/request-param-v3")
+    @RequestMapping ( "/request-param-v3" )
     public String requestParamV3(
-            @RequestParam("username") String username,
-            @RequestParam("age") int age
+            @RequestParam ( "username" ) String username,
+            @RequestParam ( "age" ) int age
     ) {
         log.info("username={}, age={}", username, age);
         return "ok";
@@ -58,10 +60,10 @@ public class RequestParamController {
     //    애노테이션에 이름을 생략하지 않고 다음과 같이 이름을 항상 적어준다. **이 방법을 권장한다.**
     //            `@RequestParam("username") String username` `@PathVariable("userId") String userId
     @ResponseBody
-    @RequestMapping("/request-param-v4")
+    @RequestMapping ( "/request-param-v4" )
     public String requestParamV4(
-            @RequestParam("username") String username,
-            @RequestParam("age") int age
+            @RequestParam ( "username" ) String username,
+            @RequestParam ( "age" ) int age
     ) {
         log.info("username={}, age={}", username, age);
         return "ok";
@@ -73,10 +75,10 @@ public class RequestParamController {
     //    애노테이션에 이름을 생략하지 않고 다음과 같이 이름을 항상 적어준다. **이 방법을 권장한다.**
     //            `@RequestParam("username") String username` `@PathVariable("userId") String userId
     @ResponseBody
-    @RequestMapping("/request-param-required")
+    @RequestMapping ( "/request-param-required" )
     public String requestParamRequired(
-            @RequestParam(name = "username", required = true) String username,
-            @RequestParam(name = "age", required = false) Integer age
+            @RequestParam ( name = "username", required = true ) String username,
+            @RequestParam ( name = "age", required = false ) Integer age
     ) {
         log.info("username={}, age={}", username, age);
         return "ok";
@@ -88,10 +90,10 @@ public class RequestParamController {
     //    애노테이션에 이름을 생략하지 않고 다음과 같이 이름을 항상 적어준다. **이 방법을 권장한다.**
     //            `@RequestParam("username") String username` `@PathVariable("userId") String userId
     @ResponseBody
-    @RequestMapping("/request-param-default")
+    @RequestMapping ( "/request-param-default" )
     public String requestParamDefault(
-            @RequestParam(name = "username", required = true, defaultValue = "guest") String username,
-            @RequestParam(name = "age", required = false, defaultValue = "-1") int age
+            @RequestParam ( name = "username", required = true, defaultValue = "guest" ) String username,
+            @RequestParam ( name = "age", required = false, defaultValue = "-1" ) int age
     ) {
         log.info("username={}, age={}", username, age);
         return "ok";
@@ -104,9 +106,27 @@ public class RequestParamController {
     //    애노테이션에 이름을 생략하지 않고 다음과 같이 이름을 항상 적어준다. **이 방법을 권장한다.**
     //            `@RequestParam("username") String username` `@PathVariable("userId") String userId
     @ResponseBody
-    @RequestMapping("/request-param-map")
+    @RequestMapping ( "/request-param-map" )
     public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
         log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping ( "/model-attribute-v1" )
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping ( "/model-attribute-v2" )
+    public String modelAttributeV2(HelloData helloData) {
+
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+
         return "ok";
     }
 }
